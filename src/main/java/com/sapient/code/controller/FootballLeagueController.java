07e8668ce54f06805d0b612bbca8b5deab0140a7
Aditya Pareek01 <adityapareek16@gmail.com>
2021-04-27 08:52:03 +0530

    few controller changes

--- src/main/java/com/sapient/code/controller/FootballLeagueController.java
+++ src/main/java/com/sapient/code/controller/FootballLeagueController.java
@@ -3,7 +3,6 @@ package com.sapient.code.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,7 @@ import com.sapient.code.model.FootballTeamStanding;
 import com.sapient.code.service.FootballLeagueService;
 
 @RestController
-@RequestMapping("/api/v1/football/standing")
+@RequestMapping("/api/v1/football/")
 public class FootballLeagueController {
 	
 	@Autowired
@@ -20,7 +19,7 @@ public class FootballLeagueController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(FootballLeagueController.class);
 	
-	@GetMapping(produces = {"application/json" })
+	@RequestMapping("standing")
 	public FootballTeamStanding getStanding(@RequestParam("countryName") String countryName, 
 			@RequestParam("leagueName") String leagueName, @RequestParam("teamName") String teamName) {
 		logger.info("getting standing for input countryName:"+countryName+",leagueName:"+leagueName+" and teamName:"+teamName);
